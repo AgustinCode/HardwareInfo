d1100850c52a6dd2feb05d0fb71e431b66753cca
AgustinCode <155396030+AgustinCode@users.noreply.github.com>
2024-07-16 22:55:31 +0200

    Update Mboard.h

--- HardwareInfo/Mboard.h
+++ HardwareInfo/Mboard.h
@@ -13,13 +13,12 @@ private:
 public:
     Mboard(); // Constructor
 
-    // Métodos
     void CollectInfo();
     std::string getManufacturer() const;
     std::string getModel() const;
     std::string getChipset() const;
     std::string getBIOSVersion() const;
-    void printInfo() const; // Para depuración
+    void printInfo() const; // For depuration
 };
 
 #endif // MBOARD_H
